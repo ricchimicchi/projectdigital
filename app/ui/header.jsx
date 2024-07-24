87aa66685f4247c34d9165422668d94beaab3a4c
rrecord <137208283+rrecord@users.noreply.github.com>
2024-07-24 22:58:41 +0400

    Project uspdate

--- app/ui/header.jsx
+++ app/ui/header.jsx
@@ -2,14 +2,20 @@ import Image from "next/image"
 import Link from "next/link"
 
 
+
 const Header = () => {
     return (
         <>
             <header className="backdrop-blur-md py-2 px-3">
-                <div className="max-w-[1300px] mx-auto">
+                <div className="max-w-[1300px] mx-auto flex items-center justify-between">
                     <Link href={'/'}>
                         <Image src='/header_logo.svg' width={50} height={50} alt="header_logo" />
                     </Link>
+                    <div className="flex items-center gap-12">
+                        <Link className="text-white" href={'/courses'}>
+                        Courses
+                        </Link>
+                    </div>
                 </div>
             </header>
         </>
