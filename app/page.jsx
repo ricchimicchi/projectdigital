35e210a444c170ce1ad3c7a1287306ef786e6ee0
rrecord <137208283+rrecord@users.noreply.github.com>
2024-07-25 01:05:59 +0400

    lele

--- app/page.jsx
+++ app/page.jsx
@@ -7,12 +7,24 @@ export default function Home() {
   return (
     <div className="back_main">
       <section className="mt-12 relative">
-      <Image src='/back-hero.svg' className="absolute 3xl:min-h-[700px] inset-0 hidden dark:block" width={3000} height={1000} alt="back"/>
-      <Image src='/back-hero-light.svg' className="absolute 3xl:min-h-[700px] inset-0 block dark:hidden" width={3000} height={1000} alt="back"/>
-        <span className={`${inter.className} dark:text-white/50 text-sm font-medium block text-center `}>A STEP-BY-STEP PATH</span>
+        <Image 
+          src='/back-hero.svg' 
+          className="absolute inset-0 object-cover h-[1000px] hidden dark:block"
+          width={3000}
+          height={1000} 
+          alt="back"
+        />
+        <Image 
+          src='/back-hero-light.svg' 
+          className="absolute inset-0 object-cover h-[1000px] block dark:hidden"
+          width={3000}
+          height={1000} 
+          alt="back"
+        />
+        <span className={`${inter.className} dark:text-white/50 text-sm font-medium block text-center`}>
+          A STEP-BY-STEP PATH
+        </span>
       </section>
-   
-      
     </div>
   );
 }
