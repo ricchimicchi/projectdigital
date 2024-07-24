76c90f6e2b7a3fc84ec7e5eb45870c55277acb91
rrecord <137208283+rrecord@users.noreply.github.com>
2024-07-25 01:15:17 +0400

    lala

--- app/page.jsx
+++ app/page.jsx
@@ -6,24 +6,27 @@ const inter = Inter_Tight({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div className="back_main">
-      <section className="mt-12 relative">
-        <Image 
-          src='/back-hero.svg' 
-          className="absolute inset-0 object-cover h-[1000px] hidden dark:block"
+      <div class="mask-t pointer-events-none h-[84px] w-full select-none backdrop-blur-[1px]"></div>
+      <section className="relative -mt-20">
+        <Image
+          src='/back-hero.svg'
+          className="absolute pointer-events-none inset-0 object-cover h-[1000px] 2xl:-mt-8 hidden dark:block opacity-40 2xl:opacity-80"
           width={3000}
-          height={1000} 
+          height={1000}
           alt="back"
         />
-        <Image 
-          src='/back-hero-light.svg' 
-          className="absolute inset-0 object-cover h-[1000px] block dark:hidden"
+        <Image
+          src='/back-hero-light.svg'
+          className="absolute pointer-events-none inset-0 object-cover h-[1000px] 2xl:-mt-8 block dark:hidden"
           width={3000}
-          height={1000} 
+          height={1000}
           alt="back"
         />
-        <span className={`${inter.className} dark:text-white/50 text-sm font-medium block text-center`}>
-          A STEP-BY-STEP PATH
-        </span>
+        <div className="pt-20">
+          <span className={`${inter.className} dark:text-white/50 text-sm font-medium block text-center`}>
+            A STEP-BY-STEP PATH
+          </span>
+        </div>
       </section>
     </div>
   );
