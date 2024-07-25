589d2d3535ddd6a7aade615895680b54a224248b
rrecord <137208283+rrecord@users.noreply.github.com>
2024-07-25 23:45:15 +0400

    kakaka

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import { Inter_Tight, Anton } from "next/font/google";
 import Image from "next/image";
+import Link from "next/link";
 
 const inter = Inter_Tight({ subsets: ["latin"] });
 const anton = Anton({ subsets: ["latin"], weight: ['400'] });
@@ -7,7 +8,7 @@ const anton = Anton({ subsets: ["latin"], weight: ['400'] });
 export default function Home() {
   return (
     <div className="back_main">
-      <div class="mask-l dark:mask-t pointer-events-none h-[84px] 1xl:h-[60px] relative z-50 w-full select-none backdrop-blur-[1px]"></div>
+      <div class="mask-l dark:mask-t pointer-events-none h-[84px] 1xl:h-[60px] relative z-50 w-full select-none backdrop-blur-[1px] transition-all"></div>
       <section className="relative -mt-20 2xl:-mt-10">
         <Image
           src='/back-hero.svg'
@@ -29,7 +30,28 @@ export default function Home() {
           </span>
           <h1 className={`${anton.className} text-center !leading-[58px] headline text-6xl 1xl:text-5xl font-bold text-white dark:block hidden`}>2024 IS YOUR LAST CHANCE</h1>
           <h1 className={`${anton.className} text-center !leading-[58px] text-6xl 1xl:text-5xl font-bold text-black dark:hidden`}>2024 IS YOUR LAST CHANCE</h1>
-          <p className={`${inter.className} dark:text-white/50 font-medium text-center mb-3`}>We have already passed the halfway point of 2024, and you! You must change your life now.</p>
+          <p className={`${inter.className} dark:text-white/50 font-medium text-center mt-2 mb-3`}>We have already passed the halfway point of 2024, and you! <br className="2xl:hidden" /> You must change your life now.</p>
+          <div className="mt-7 flex items-center justify-center">
+            <Image
+              src='/hero-main-images/banner.webp'
+              className="pointer-events-none rounded-2xl 3xl:hidden"
+              width={1000}
+              height={1000}
+              alt="back"
+            />
+             <Image
+              src='/hero-main-images/banner-mobile.svg'
+              className="pointer-events-none rounded-2xl hidden 3xl:block"
+              width={1000}
+              height={1000}
+              alt="back"
+            />
+          </div>
+          <div className="flex items-center justify-center mt-4">
+            <Link href={'/'} className={`${inter.className} font-medium text-sm block text-center mb-3 px-4 py-2 rounded-xl border-[1px] border-black bg-black text-white hover:bg-transparent hover:text-black trs_lg dark:bg-white dark:text-black dark:hover:bg-transparent dark:hover:border-white dark:hover:text-white`}>
+              Get Started
+            </Link>
+          </div>
         </div>
       </section>
     </div>
